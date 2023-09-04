2dc225cd8c01003662f1058b2553ec6d11050e8d
Ms-Myo-Myo-Khant <103840550+Ms-Myo-Myo-Khant@users.noreply.github.com>
2023-09-05 03:59:18 +0630

    Users Database Created

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Users(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=128)  # Store hashed passwords securely
+    gender = models.CharField(max_length=10, choices=[('male', 'Male'), ('female', 'Female')])
+
+    def __str__(self):
+        return self.username
--- signup/views.py
+++ signup/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from .forms import CustomSignupForm
+from .models import Users
 
 def get_signup(request):
-    return HttpResponse('This is Singup page frame!', content_type='text/plain')
+    if request.method == 'POST':
+        form = CustomSignupForm(request.POST)
+        if form.is_valid():
+            # Create a UserProfile instance and save it
+            user_profile = Users(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=make_password(form.cleaned_data['password']),  # Hash the password
+                gender=form.cleaned_data['gender']
+            )
+            user_profile.save()
+            return redirect('index')  # Replace 'index' with your desired success page
+    else:
+        form = CustomSignupForm()
+    return render(request, 'signup/signup.html', {'form': form})
