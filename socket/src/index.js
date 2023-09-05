3c79f95af382c1e0462c428f85058a60bb0250c7
Sai Htet Aung <saihtetaung@tunyat.com>
2023-09-05 19:41:03 +0630

    node fix

--- socket/src/index.js
+++ socket/src/index.js
@@ -1,9 +1,15 @@
 const http = require('http');
 const dotEnv = require('dotenv');
 dotEnv.config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ origin: '*' }));
+app.all('/', (req, res) => res.end('This is Hi-Find Socket Proxy'));
 
 const PORT = process.env['PORT'] || 3000;
-const server = http.createServer((req, res) => res.end('Hello! This is Socket Proxy server for Hi-Find'));
+const server = http.createServer(app);
 const socket = require('./socket');
 socket.config(server);
 
